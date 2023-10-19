6e805e52491fdf805ac5db34b08022a1a70a512b
gabrielvictorcf <gabrielvcf@outlook.com>
2023-10-18 21:28:23 -0300

    better path print + use a* pathfinding in main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import utils
-from bfs import bfs
+from bfs import bfs, a_star
 
-stops = utils.load_gtfs("data/stops.txt", set([0,1]))
+stops = utils.load_gtfs("data/stops.txt", set([0,1,2,3]))
 trips = utils.load_gtfs("data/stop_times.txt", set([0,1,2,3]))
 transfers = utils.load_gtfs("data/transfers.txt", set([0,1]))
 graph = utils.create_graph(stops, trips, transfers)
 
 path = bfs(graph, '101', 'F24')
-print(path)
+print(f"bfs: {' '.join(list(map(lambda x: str(x), path)))}")
+
+path = a_star(graph, '101', 'F24')
+print(f"a_star: {' '.join(list(map(lambda x: str(x), path)))}")
